70b84258e8162ae9cc5fa4ea0d6c15a5f15ee455
courtneywmorris <courtneywmorris@gmail.com>
2016-06-30 10:53:41 -0700

    updated clean and nmf scripts

--- code/clean_data.py
+++ code/clean_data.py
@@ -29,6 +29,25 @@ def time_period_clean(df, start_date, end_date):
 
     return df
 
+def non_popular_clean(df, upper_limit, lower_limit):
+    df['new_name'] = df.Name.str.lower()
+    df = df.set_index(['Year','State'])
+    df['name_gender'] = zip(df.new_name, df.Gender)
+    df = df.drop(['Year', 'State', 'new_name', 'Id', 'Name'])
+
+    date_state_series = df.groupby(df.index).sum()['Count']
+    df['tuple_index'] = df.index.values
+    df = df.join(date_state_series, how='left', on='tuple_index', lsuffix='_by_name')
+
+    df['count_norm'] = df.Count_by_name / df.Count
+
+    names_total = df.groupby('name_gender')['Count_name_count'].sum()
+    df = df.join(names_total, how='left', on='name_gender', lsuffix='_per_state')
+    df = df[(df['Count_name_count'] <= upper_limit) & (df['Count_name_count'] >= lower_limit)]
+
+    return df
+
+
 def pivot_data(df, pivot_vals, pivot_index, pivot_cols):
     data_pivot = pd.pivot_table(df, values=pivot_vals, index=pivot_index, columns=pivot_cols)
     data_pivot = data_pivot.fillna(0)
--- code/run_nmf.py
+++ code/run_nmf.py
@@ -6,7 +6,7 @@ def run_nmf(df,params):
     nmf = NMF(n_components=params)
     W = nmf.fit_transform(df)
     H = nmf.components_
-    return W, H, nmf, nmf.reconstruction_err_
+    return W, H, nmf
 
 def recon_error(df, k):
     nmf = NMF(n_components=k)
@@ -14,7 +14,7 @@ def recon_error(df, k):
     H = nmf.components_
     return nmf.reconstruction_err_
 
-def reconst_mse(target, left, right):
+def reconst_error_sum(target, left, right):
     return (np.array(target - left.dot(right))**2).sum()
 
 def print_top_features(W, H, features, top_features, data_matrix):
@@ -23,6 +23,16 @@ def print_top_features(W, H, features, top_features, data_matrix):
         print "Latent Feature %d" % (int(latent_num) + 1)
         print [features[i] for i in latent.argsort()[:-top_features-1:-1]]
         print
+
+def print_year_diff_features(W, H, features, top_features, data_matrix):
+    print "Reconstruction Error: %f" %reconst_mse(data_matrix, W, H)
+    for latent_num, latent in enumerate(H):
+        print "Latent Feature %d" % (int(latent_num) + 1)
+        results = [features[i] for i in latent.argsort()[:-top_features-1:-1]]
+        result_years = [features[i][0] for i in latent.argsort()[:-top_features-1:-1]]
+        year_diff = max(result_years) - min(result_years)
+        if year_diff > 10:
+            print results
 
 def run_decade_nmf(df, (start_date, end_date), n_factors):
     decade_df = time_period_clean(df, start_date, end_date)
@@ -43,6 +53,15 @@ def run_pop_nmf(df, n_factors):
     state_features = df.columns
 
     print "SKlearn error: %f" %error
-    print print_top_features(W,H,state_features,20,df)
+    print print_year_diff_features(W,H,state_features,20,df)
+
+    return W, H, nmf
+
+def run_tfidf_nmf(array, n_factors, features):
+
+    W, H, nmf, error = run_nmf(df,n_factors)
+
+    print "SKlearn error: %f" %error
+    print print_top_features(W,H,features,20,array)
 
     return W, H, nmf
