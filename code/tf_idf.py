290784bb4fecda6fa3cf97f3d1c26632fda6eed1
courtneywmorris <courtneywmorris@gmail.com>
2016-07-03 16:10:43 -0700

    Interim save

--- code/tf_idf.py
+++ code/tf_idf.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import numpy as np
 from clean_data import clean_data_from_frame
+from sklearn.feature_extraction.text import TfidfTransformer
 
 def tf_idf_matrix(df, gender):
     df = clean_data_from_frame(df)
     gender_frame = df[df['Gender']==gender]
 
     pivot = pd.pivot_table(gender_frame, values = 'Count_by_name',
-    index = gender_frame.tuple_index, columns= gender_frame.Name)
+    index = 'tuple_index', columns= 'Name')
 
     doc_freq = pivot.count()
     max_values = pivot.max(axis=1)
@@ -18,6 +19,9 @@ def tf_idf_matrix(df, gender):
     tf_idf_frame = my_tfidf(clean_pivot, pivot.shape[0],doc_freq)
 
     return tf_idf_frame
+
+def count_transform(df, ):
+
 
 def my_tfidf(df, n_documents, doc_freq_series):
     column_idf = np.log((n_documents + 1.0) / (doc_freq_series + 1.0)) + 1.0
